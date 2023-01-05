b6137288164e2660e103c288e05c4b89ad89b1bc
edkardoso <edkardoso@gmail.com>
2023-01-05 06:36:26 -0300

    Melhorando o UseCase e adicionando o Presenter

---
+++ src/edk.kchef.domain/Common/IPresenter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using FluentValidation.Results;
+
+namespace edk.Kchef.Domain.Common
+{
+    public interface IPresenter<TInput, TOutput>
+    {
+        public abstract void OnError(TInput input, ValidationResult validationResult);
+
+        public abstract void OnException(TInput input, Exception exception);
+
+        public abstract void OnSuccess(TOutput output);
+
+        public TOutput Result { get; }
+
+        public dynamic ViewResult { get; }
+    }
+}
--- src/edk.kchef.domain/Common/UseCase.cs
+++ src/edk.kchef.domain/Common/UseCase.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using FluentValidation;
 using FluentValidation.Results;
 
@@ -11,56 +7,60 @@ namespace edk.Kchef.Domain.Common
     public abstract class UseCase<TInput, TOutput>
     {
         private readonly AbstractValidator<TInput> _validator;
+        private readonly IPresenter<TInput, TOutput> _presenter;
         private bool _isValid = true;
         private bool _complete;
         private ValidationResult _validationResult;
-        public UseCase(AbstractValidator<TInput> validator = default)
+
+    
+        public UseCase(AbstractValidator<TInput> validator = default, IPresenter<TInput, TOutput> presenter = null)
         {
             _validator = validator;
+            _presenter = presenter;
         }
 
-        public void Execute(TInput input)
+        public IPresenter<TInput, TOutput> Execute(TInput input)
         {
             try
             {
                 if (_validator != null)
                 {
-                     _validationResult = _validator.Validate(input);
+                    _validationResult = _validator.Validate(input);
                     _isValid = _validationResult.IsValid;
                 }
 
                 if (_isValid)
                 {
-                    OnExecute(input, _validationResult);
+                    var result = OnExecute(input, _validationResult);
                     _complete = true;
+                    _presenter.OnSuccess(result);
                 }
                 else
                 {
-                    OnNotIsValid(input, _validationResult);
+                    _presenter.OnError(input, _validationResult);
                 }
 
             }
             catch (Exception ex)
             {
+                // Pode ser necessário adicionar um novo evento virtual de Exceção
+                // para cenários onde há a necessidade de se desfazer alguma ação anterior
+                // em caso de exceção. Embora o OnComplete(false) talvez já permita isso.
 
-                OnException(ex);
+                _presenter.OnException(input, ex);
+
             }
             finally
             {
                 OnComplete(_complete);
             }
-        }
-        public abstract void OnExecute(TInput input, ValidationResult validationResult);
-        public virtual void OnNotIsValid(TInput input, ValidationResult validationResult)
-        {
 
+            return _presenter;
 
         }
 
-        public virtual void OnException(Exception exception)
-        {
-            throw exception;
-        }
+        public abstract TOutput OnExecute(TInput input, ValidationResult validationResult);
+
 
         public virtual void OnComplete(bool completed)
         {
