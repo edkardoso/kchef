9df12e98cdbaee31ab78fe9e3fba860f98377853
edkardoso <edkardoso@gmail.com>
2023-02-17 21:05:13 -0300

    Modelo de item template do UseCase

---
+++ src/edk.kchef.application/_templates/ItemTemplateUseCase.cs
@@ -0,0 +1,16 @@
+﻿//using System;
+//using System.Threading;
+//using System.Threading.Tasks;
+//using edk.Fusc.Core;
+
+//namespace $rootnamespace$;
+//public class $safeitemname$UseCase : UseCase<$safeitemname$Input, $safeitemname$Output>
+//{
+//    protected override string NameUseCase => "$safeitemname$";
+
+//    public override Task<$safeitemname$Output> OnExecuteAsync($safeitemname$Input input, CancellationToken cancellationToken)
+//    {
+//       throw new NotImplementedException(); 
+//    }
+//}
+
