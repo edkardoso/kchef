d8f2b0d7940fbd322cef6d8f12e1dcd53e2a5906
Eduardo Cardoso <eduardo.cardoso@ciandt.com>
2023-02-19 12:46:06 -0300

    Classe abstrata base para o Value Object

---
+++ src/edk.kchef.domain/Common/ValueObjects/FullNameVO.cs
@@ -0,0 +1,40 @@
+﻿using edk.Kchef.Domain.Entities.Users;
+using System.Collections.Generic;
+
+namespace edk.Kchef.Domain.Common.ValueObjects
+{
+    public class FullNameVO : ValueObject
+    {
+        public string FirstName { get; }
+        public string MiddleName { get; }
+        public string LastName { get; }
+        public GenderType Gender { get; }
+
+
+        public FullNameVO(string firstName, string lastName, string middleName, GenderType gender)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Gender = gender;
+            MiddleName = middleName;
+        }
+
+        protected override IEnumerable<object> GetEqualityComponents()
+        {
+            yield return FirstName;
+            yield return LastName;
+            yield return Gender;
+        }
+
+        public string Treatment() => Gender switch
+        {
+            GenderType.Male => "Sr. ",
+            GenderType.Female => "Sra. ",
+            _ => string.Empty
+        };
+
+        public override string ToString() => $"{Treatment()}{FirstName} {LastName}";
+
+
+    }
+}
