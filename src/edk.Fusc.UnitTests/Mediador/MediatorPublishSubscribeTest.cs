d410f4cc0b28494588fe827268e0f5b5f6eb7508
Eduardo Cardoso <eduardo.cardoso@ciandt.com>
2023-02-25 20:27:51 -0300

    Testes do mediator

---
+++ src/edk.Fusc.UnitTests/Mediador/MediatorPublishSubscribeTest.cs
@@ -0,0 +1,87 @@
+﻿using edk.Fusc.Core.Mediator;
+using edk.Fusc.Core;
+using edk.Fusc.Core.Events;
+using edk.Fusc.Contracts;
+
+namespace edk.Fusc.UnitTests.Mediador
+{
+    public class MediatorPublishSubscribeTest
+    {
+
+        [Fact]
+        public async Task ShouldSubscribePublishUseCase()
+        {
+            // arrange
+            var mediator = new UseCaseMediator();
+            var useCaseSender = new UseCase1(mediator);
+            var useCaseObserver = new UseCase2(mediator);
+            var eventStart = new UseCaseStartEvent(useCaseSender);
+
+            // action
+            mediator.Subscribe<UseCaseStartEvent, UseCase1>(useCaseObserver);
+
+            // assert
+            mediator.Publish(eventStart);
+
+        }
+
+        [Fact]
+        public async Task ShouldSubscribePublishUseCase2()
+        {
+            // arrange
+            var mediator = new UseCaseMediator();
+            var useCase1 = new UseCase1(mediator);
+            var useCase2 = new UseCase2(mediator);
+            var eventStart = new UseCaseStartEvent(useCase1);
+
+            // action
+            useCase1.HandleAsync(string.Empty);
+
+            // assert
+            mediator.Publish(eventStart);
+
+        }
+
+        private class UseCase1 : UseCase<string, string>
+        {
+            public UseCase1(IMediatorUseCase mediator)
+               : base(mediator)
+            { }
+
+            protected override string NameUseCase => throw new NotImplementedException();
+
+            public override Task<string> OnExecuteAsync(string input, CancellationToken cancellationToken)
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        private class UseCase2 : UseCase<string, string>
+        {
+            protected override string NameUseCase => throw new NotImplementedException();
+
+            public UseCase2(IMediatorUseCase mediator)
+                : base(mediator)
+            {}
+
+
+            public override Task<string> OnExecuteAsync(string input, CancellationToken cancellationToken)
+            {
+                return Task.FromResult(input);
+            }
+
+
+            public override Task OnEventAsync<TEvent>(TEvent useCaseEvent)
+            {
+                return base.OnEventAsync(useCaseEvent);
+            }
+
+
+
+
+
+        }
+
+
+    }
+}
--- src/edk.Fusc.UnitTests/Mediador/MediatorTest.cs
+++ src/edk.Fusc.UnitTests/Mediador/MediatorTest.cs
@@ -1,85 +1,41 @@
 ﻿using edk.Fusc.Core.Mediator;
 using edk.Fusc.Core;
-using edk.Fusc.Core.Events;
 using edk.Fusc.Contracts;
+using Moq;
 
 namespace edk.Fusc.UnitTests.Mediador
 {
-    public  class MediatorTest
+
+    public class MediatorTest
     {
-
         [Fact]
-        public async Task ShouldSubscribePublishUseCase()
+        public void ShouldReturnAnInstanceOfMyUseCase()
         {
             // arrange
-            var mediator = new UseCaseMediator();
-            var useCaseSender = new UseCase1(mediator);
-            var useCaseObserver = new UseCase2(mediator);
-            var eventStart = new UseCaseStartEvent(useCaseSender);
-
-            // action
-            mediator.Subscribe<UseCaseStartEvent, UseCase1>(useCaseObserver);
-
+            var factoryMediator = new Mock<IFactoryMediator>();
+            var mediator = new UseCaseMediator(factoryMediator.Object);
+            factoryMediator.Setup(s => s.Get<MyUseCase>())
+                            .Returns(new MyUseCase(mediator));
+            // act
+            var useCase = mediator.Factory.Get<MyUseCase>();
+            
             // assert
-            mediator.Publish(eventStart);
+            Assert.NotNull(useCase);
 
         }
 
-        [Fact]
-        public async Task ShouldSubscribePublishUseCase2()
+        private class MyUseCase : UseCase<NoValue, NoValue>
         {
-            // arrange
-            var mediator = new UseCaseMediator();
-            var useCase1 = new UseCase1(mediator);
-            var useCase2 = new UseCase2(mediator);
-            var eventStart = new UseCaseStartEvent(useCase1);
-
-            // action
-            useCase1.HandleAsync(string.Empty);
-
-            // assert
-            mediator.Publish(eventStart);
-
-        }
-
-        private class UseCase1 : UseCase<string, string>
-        {
-            public UseCase1(IMediatorUseCase mediator)
+            public MyUseCase(IMediatorUseCase mediator)
                : base(mediator)
             { }
 
             protected override string NameUseCase => throw new NotImplementedException();
 
-            public override Task<string> OnExecuteAsync(string input, CancellationToken cancellationToken)
+            public override Task<NoValue> OnExecuteAsync(NoValue input, CancellationToken cancellationToken)
             {
-                throw new NotImplementedException();
+                return Task.FromResult(default(NoValue));
             }
-        }
-
-        private class UseCase2 : UseCase<string, string>
-        {
-            protected override string NameUseCase => throw new NotImplementedException();
-
-            public UseCase2(IMediatorUseCase mediator)
-                : base(mediator)
-            {}
-
-
-            public override Task<string> OnExecuteAsync(string input, CancellationToken cancellationToken)
-            {
-                return Task.FromResult(input);
-            }
-
-
-            public override Task OnEventAsync<TEvent>(TEvent useCaseEvent)
-            {
-                return base.OnEventAsync(useCaseEvent);
-            }
-
-
-
-
-
         }
 
 
