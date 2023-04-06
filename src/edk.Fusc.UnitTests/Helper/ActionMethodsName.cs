dca44d1fb120314c48a9c552d083f5ce5fe7bfce
Eduardo Cardoso <eduardo.cardoso@ciandt.com>
2023-04-06 16:42:06 -0300

    Ajustando os testes unitários

--- src/edk.Fusc.UnitTests/Helper/ActionMethodsName.cs
+++ src/edk.Fusc.UnitTests/Helper/ActionMethodsName.cs
@@ -3,7 +3,7 @@
 public static class ActionMethodsName
 {
 
-    public static string OnActionBeforeStart = "OnActionBeforeStart";
+    public static string OnActionBeforeStartAsync = "OnActionBeforeStartAsync";
     public static string OnExecuteAsync = "OnExecuteAsync";
     public static string OnActionException = "OnActionException";
     public static string OnActionComplete = "OnActionComplete";
--- src/edk.Fusc.UnitTests/UseCase/FlowUseCasesTests.cs
+++ src/edk.Fusc.UnitTests/UseCase/FlowUseCasesTests.cs
@@ -10,33 +10,49 @@ namespace edk.Fusc.UnitTests.UseCase;
 
 public class FlowUseCasesTests
 {
+    public class UseCaseFlowTest : UseCase<string, bool>
+    {
+        protected override string NameUseCase => throw new NotImplementedException();
+
+        public override Task<bool> OnExecuteAsync(string? input, CancellationToken cancellationToken)
+        {
+            if(input == "error")
+            {
+                throw new ArgumentException(nameof(input));
+            }
+
+            return Task.FromResult(true);
+        }
+    }
+
+
     [Fact]
     public async Task ShouldOnlyInvokeMethodsOfNormalFlow()
     {
         // arrange
-        var useCaseMock = new Mock<UseCase<NoValue, bool>>() { CallBase = true }; // callbase=true não substitui o método deixando o comportamento padrão
+        var useCaseMock = new Mock<UseCaseFlowTest>() { CallBase = true }; // callbase=true não substitui o método deixando o comportamento padrão
         var useCase = useCaseMock.Object;
 
         // action
         _ = await useCase.HandleAsync();
 
         // assert
-        useCaseMock.Protected().Verify(ActionMethodsName.OnActionBeforeStart, Times.Once(), ItExpr.IsAny<NoValue>(), ItExpr.IsAny<IUser>());
-        useCaseMock.Verify(s => s.OnExecuteAsync(It.IsAny<NoValue>(), It.IsAny<CancellationToken>()), Times.Once());
+        useCaseMock.Protected().Verify(ActionMethodsName.OnActionBeforeStartAsync, Times.Once(), ItExpr.IsAny<string>(), ItExpr.IsAny<IUser>());
+        useCaseMock.Verify(s => s.OnExecuteAsync(It.IsAny<string>(), It.IsAny<CancellationToken>()), Times.Once());
         useCaseMock.Protected().Verify(ActionMethodsName.OnActionComplete, Times.Once(), ItExpr.IsAny<bool>(), ItExpr.IsAny<IReadOnlyCollection<INotification>>());
-        useCaseMock.Protected().Verify(ActionMethodsName.OnActionException, Times.Never(), ItExpr.IsAny<Exception>(), ItExpr.IsAny<NoValue>(), ItExpr.IsAny<IUser>());
+        useCaseMock.Protected().Verify(ActionMethodsName.OnActionException, Times.Never(), ItExpr.IsAny<List<Exception>>(), ItExpr.IsAny<string>(), ItExpr.IsAny<IUser>());
 
     }
 
 
     [Fact]
-    public async Task MustStopExecutionAndNotInvokeAnotherMethodWhenOnActionBeforeStartReturnFalse()
+    public async Task OnActionBeforeStart_WhenReturnFalse_MustStopExecution()
     {
         // arrange
-        var useCaseMock = new Mock<UseCase<NoValue, bool>>() { CallBase = true };
+        var useCaseMock = new Mock<UseCaseFlowTest>() { CallBase = true };
         useCaseMock.Protected()
-            .Setup<bool>(ActionMethodsName.OnActionBeforeStart, ItExpr.IsAny<NoValue>(), ItExpr.IsAny<UserNull>())
-            .Returns(false);
+            .Setup<Task<bool>>(ActionMethodsName.OnActionBeforeStartAsync, ItExpr.IsAny<string>(), ItExpr.IsAny<UserNull>())
+            .Returns(Task.FromResult(false));
 
         var useCase = useCaseMock.Object;
 
@@ -44,25 +60,25 @@ public class FlowUseCasesTests
         _ = await useCase.HandleAsync();
 
         // assert
-        useCaseMock.Verify(s => s.OnExecuteAsync(It.IsAny<NoValue>(), It.IsAny<CancellationToken>()), Times.Never());
+        useCaseMock.Verify(s => s.OnExecuteAsync(It.IsAny<string>(), It.IsAny<CancellationToken>()), Times.Never());
         useCaseMock.Protected().Verify(ActionMethodsName.OnActionComplete, Times.Never(), ItExpr.IsAny<bool>(), ItExpr.IsAny<IReadOnlyCollection<Notification>>());
-        useCaseMock.Protected().Verify(ActionMethodsName.OnActionException, Times.Never(), ItExpr.IsAny<Exception>(), ItExpr.IsAny<NoValue>(), ItExpr.IsAny<IUser>());
+        useCaseMock.Protected().Verify(ActionMethodsName.OnActionException, Times.Never(), ItExpr.IsAny<List<Exception>>(), ItExpr.IsAny<string>(), ItExpr.IsAny<IUser>());
     }
 
     [Fact]
     public async Task ShouldInvokeAllMethodsToExceptionFlowWhenAnExceptionOccursInOnExecuteAsync()
     {
         // arrange
-        var useCaseMock = new Mock<UseCase<NoValue, string>>() { CallBase = true };
-        useCaseMock.Setup(s => s.OnExecuteAsync(It.IsAny<NoValue>(), It.IsAny<CancellationToken>())).Throws<Exception>();
+        var useCaseMock = new Mock<UseCaseFlowTest>() { CallBase = true };
+        useCaseMock.Setup(s => s.OnExecuteAsync(It.IsAny<string>(), It.IsAny<CancellationToken>())).Throws<Exception>();
         var useCase = useCaseMock.Object;
 
         // action
         var presenter = await useCase.HandleAsync();
 
         // assert
-        useCaseMock.Protected().Verify(ActionMethodsName.OnActionBeforeStart, Times.Once(), ItExpr.IsAny<NoValue>(), ItExpr.IsAny<IUser>());
-        useCaseMock.Protected().Verify(ActionMethodsName.OnActionException, Times.Once(), ItExpr.IsAny<Exception>(), ItExpr.IsAny<NoValue>(), ItExpr.IsAny<IUser>());
+        useCaseMock.Protected().Verify(ActionMethodsName.OnActionBeforeStartAsync, Times.Once(), ItExpr.IsAny<string>(), ItExpr.IsAny<IUser>());
+        useCaseMock.Protected().Verify(ActionMethodsName.OnActionException, Times.Once(), ItExpr.IsAny<List<Exception>>(), ItExpr.IsAny<string>(), ItExpr.IsAny<IUser>());
         useCaseMock.Protected().Verify(ActionMethodsName.OnActionComplete, Times.Once(), ItExpr.IsAny<bool>(), ItExpr.IsAny<IReadOnlyCollection<INotification>>());
     }
 
